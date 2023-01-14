bcd553a6da75078445d26201747fb22fe8a2ddbd
zlh <1208264396@qq.com>
2023-01-14 21:20:18 +0800

    feat: throttle

--- index.js
+++ index.js
@@ -48,3 +48,49 @@ const debounce = (func, wait, immediate) => {
 
   return debounced;
 };
+
+/**
+ * 节流
+ * @param {Function} func
+ * @param {Number} wait
+ * @param {{ leading: Boolean, trailing: Boolean }} options leading: 鼠标移入是否能立刻执行  trailing: 停止触发的时是否还能再执行一次
+ * @returns
+ */
+const throttle = (func, wait, options = {}) => {
+  const { leading, trailing } = options;
+  let prevTimestamp = 0, timerId;
+
+  const throttled = function (...args) {
+    const now = Date.now();
+    if (!prevTimestamp && !leading) {
+      // 是第一次执行并且禁用
+      prevTimestamp = now; // 这样的话会使得下面的代码因为now - prevTimestamp不够wait秒而不立刻执行
+    }
+    const left = wait - (now - prevTimestamp);
+    const context = this;
+    if (left <= 0 || left > wait) {
+      if (timerId) {
+        // 因为上一秒 left <= 0 || left > wait 未满足而设置了定时器，所以要取消掉
+        clearTimeout(timerId);
+        timerId = null;
+      }
+      func.apply(context, args);
+      prevTimestamp = now;
+    } else if (!timerId && trailing) {
+      // 这里就是最后一次执行的代码
+      timerId = setTimeout(() => {
+        prevTimestamp = leading ? Date.now() : 0; // 如果是禁用第一次执行，则需要重置prevTimestamp
+        func.apply(context, args);
+        timerId = null;
+      }, wait);
+    }
+  };
+
+  throttled.cancel = function () {
+    clearTimeout(timerId);
+    prevTimestamp = 0;
+    timerId = null;
+  };
+
+  return throttled;
+};
