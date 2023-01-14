1e4253cb92c6f9a635a12390511fba3a6f8e0fcc
zlh <1208264396@qq.com>
2023-01-14 21:19:57 +0800

    feat: debounce

--- index.js
+++ index.js
@@ -7,3 +7,44 @@ const type = (obj) =>
   Reflect.apply(Object.prototype.toString, obj, [])
     .replace(/^\[object\s(\w+)\]$/, "$1")
     .toLowerCase();
+
+/**
+ * 防抖
+ * @param {Function} func
+ * @param {Number} wait
+ * @param {Boolean} immediate
+ * @returns
+ */
+const debounce = (func, wait, immediate) => {
+  let timerId, result;
+
+  const debounced = function (...args) {
+    const context = this;
+
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    if (immediate) {
+      // 如果已经执行过，不再执行
+      const callNow = !timerId;
+      timerId = setTimeout(function () {
+        timerId = null;
+      }, wait);
+      if (callNow) {
+        result = func.apply(context, args);
+      }
+    } else {
+      timerId = setTimeout(function () {
+        func.apply(context, args);
+      }, wait);
+    }
+    return result;
+  };
+
+  debounced.cancel = function () {
+    clearTimeout(timerId);
+    timerId = null;
+  };
+
+  return debounced;
+};
